06fb46756a1f560471fb43e1ce3b79dfcd5cc616
Kshamika-Ghiya <76477468+Kshamika-Ghiya@users.noreply.github.com>
2024-01-17 10:19:57 +0530

    Update pascals_triangle.py

--- Patterns - Python/pascals_triangle.py
+++ Patterns - Python/pascals_triangle.py
@@ -1,17 +1,17 @@
 def main():
     try:
-        length = int(input("Enter the length of the palindromic pyramid: "))
+        length = int(input("Enter the length of the triangle: "))
         
         if length <= 0:
             print("Please enter positive value for length.")
         else:
-            print("\nPalindromic Pyramid Pattern:")
-            palindromic(length)
+            print("\nPascal's Triangle Pattern:")
+            pascals_triangle(length)
     
     except ValueError:
         print("Invalid input. Please enter valid integer for length.")
 
-def palindromic(length):
+def pascals_triangle(length):
     for i in range(length):
         coeff = 1
         for j in range(i+1):
